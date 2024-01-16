1ee8136ce40f1804990f2e5071bd8cebed101724
zleer <baconsoups17@gmail.com>
2024-01-15 19:04:10 -0600

    Added Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public InputAction playerMovement;
+
+    Vector2 moveDirection = Vector2.zero;
+
+    private void OnEnable()
+    {
+        playerMovement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerMovement.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        moveDirection = playerMovement.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
